9ba7fd8f260fa85ea37516538dd767bdbe505706
Hedkandi <Hedkandi@users.noreply.github.com>
2017-01-31 08:11:39 +0100

    Create Get-MsiProperty.ps1

---
+++ Get-MsiProperty.ps1
@@ -0,0 +1,31 @@
+function Get-MsiProperty {
+    param(
+        [string]$MsiDatabasePath,
+        [string]$sProperty
+    )
+
+    # Ref: http://www.laurierhodes.info/?q=node/104
+
+    Write-Information "Opening '$MsiDatabasePath'"
+    if ($MsiDatabasePath) {
+        try {
+            $oDatabase = New-Object WixToolset.Dtf.WindowsInstaller.Database($MsiDatabasePath);
+        } catch {}
+        if ($oDatabase) {
+            $sSQLQuery = "SELECT * FROM Property WHERE Property = '$sProperty'"
+            [WixToolset.Dtf.WindowsInstaller.View]$oView = $oDatabase.OpenView($sSQLQuery)
+            $oView.Execute()
+
+            while ($oRecord = $oView.Fetch()) {
+                if ($oRecord.GetString(1) -eq "$sProperty") {
+                    return $oRecord.GetString(2)
+                }
+            }
+            $oView.Close()
+            $oDatabase.Dispose()
+        }
+        else {
+            throw "Failed to open database."
+        }
+    }
+}

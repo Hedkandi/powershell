ec2569ad34bc93e1a7da20cc97c6fd23a4ea6f75
Hedkandi <Hedkandi@users.noreply.github.com>
2017-08-23 09:09:34 +0200

    Update CompareVersions.ps1

--- CompareVersions.ps1
+++ CompareVersions.ps1
@@ -1,7 +1,8 @@
-$OldVersion = [System.Version]::new("0.1.0.0")
-$NewVersion = [System.Version]::new(([System.Diagnostics.FileVersionInfo]::GetVersionInfo("PATH").FileVersion))
+$cmdExe = 'C:\windows\system32\cmd.exe'
 
-Write-Host "NewVersion: $NewVersion"
-Write-Host "OldVersion: $OldVersion"
+$Version = [System.Version]::new("0.1.0.0")
+$cmdVersion = [System.Version]::new(([System.Diagnostics.FileVersionInfo]::GetVersionInfo($cmdExe).ProductVersion))
 
-($OldVersion -lt $NewVersion)
+Write-Host "Version: $Version`ncmdVersion: $cmdVersion"
+
+($cmdVersion -lt $Version)
